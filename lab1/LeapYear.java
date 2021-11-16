859d83d569567c0e1ed96b9f1adce561ac1d71f2
reiuyuu <reiuyuu@gmail.com>
2021-11-17 00:57:02 +0800

    completed LeapYear in lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,7 +1,19 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author reiuyuu
  */
+
+ /** Check the remainder in the order of 400, 100, 4 */
 public class LeapYear {
+    public static boolean isLeapYear(int year) {
+        if (year%400 == 0) {
+            return true;
+        } else if (year%100 == 0) {
+            return false;
+        } else if (year%4 == 0) {
+            return true;
+        }
+        return false;
+    }
 
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
